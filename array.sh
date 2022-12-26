5424d5c5a91fd3ec0233e6225381b0971e645dce
dimitri <korgalidzed@gmail.com>
2022-12-26 17:31:24 +0400

    First version

--- array.sh
+++ array.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 # Place your code here
+num_folders=$1
+
+folder_names=""
+
+for letter in {a..z}; do
+
+  mkdir "folder_$letter"
+
+  folder_names="$folder_names folder_$letter"
+
+  # Decrement the number of folders to create
+  num_folders=$((num_folders-1))
+
+  # If the number of folders to create is zero, exit the loop
+  if [ "$num_folders" -eq 0 ]; then
+    break
+  fi
+done
+
+# Print a message indicating the number of folders created and their names
+echo "$1 folders created:$folder_names"
